9a4b6535f5ba1c17018a5d2e265b0d2f291d69fe
redcyb <redcyb@gmail.com>
2018-03-28 16:28:52 +0300

    update class structure

--- anfis/anfis1.py
+++ anfis/anfis1.py
@@ -15,7 +15,7 @@ class ANFIS1:
     layer_1A = None
     layer_2Z = None
     layer_2A = None
-    layer_3 = None
+    layer_3A = None
 
     layer_1mfZ = None
 
@@ -46,14 +46,15 @@ class ANFIS1:
         self.memClass = copy.deepcopy(memFunction)
 
         # NEW
-
         self.l5_activation_func = l5_activation_func
         input_vars_ranges = [[X[:, i].min(), X[:, i].max()] for i in range(X.shape[1])]
 
         num_of_mfs_for_each_var = 3
 
-        self.weights_XtoMFs = [np.array(gen_mf_by_range("gaussian", num_of_mfs_for_each_var, *i)) for i in
-                               input_vars_ranges]
+        self.weights_XtoMFs = [
+            np.array(gen_mf_by_range("gaussian", num_of_mfs_for_each_var, *i))
+            for i in input_vars_ranges]
+
         self.mfs_by_vars_combinations = [[x for x in range(num_of_mfs_for_each_var)] for z in range(X.shape[1])]
         self.rules = np.array(list(itertools.product(*self.mfs_by_vars_combinations)))
 
@@ -124,7 +125,7 @@ class ANFIS1:
         self.layer_2A = self.layer_2_forward_online()
 
         # layer 3 : NORMALIZATION
-        self.layer_3 = self.layer_3_forward_online()
+        self.layer_3A = self.layer_3_forward_online()
 
         # layer 4 : TSK
         self.layer_4A = self.layer_4_forward_online(x)
@@ -138,17 +139,19 @@ class ANFIS1:
         return res
 
     def layer_1_forward_online(self, x):
-        # result1 = np.array(self.memClass.evaluate_mf(x))
-        rr = []
-        for i in range(len(x)):
-            aa = np.array(self.weights_XtoMFs[i])
-            aa[:, 0] = aa[:, 0] * x[i]
-            aa = np.sum(aa, axis=1)
-            rr.append(aa)
+        result1 = np.array(self.memClass.evaluate_mf(x))
+        # rr = []
+        # for i in range(len(x)):
+        #     aa = np.array(self.weights_XtoMFs[i])
+        #     aa[:, 0] = aa[:, 0] * x[i]
+        #     aa = np.sum(aa, axis=1)
+        #     rr.append(aa)
+        #
+        # self.layer_0A = np.append(x, 1)
+        #
+        # return [activate("gaussian", i) for i in rr]
 
-        self.layer_0A = np.append(x, 1)
-
-        return [activate("gaussian", i) for i in rr]
+        return result1
 
     def layer_2_forward_online(self):
         self.layer_2Z = [
@@ -168,10 +171,12 @@ class ANFIS1:
 
     def layer_4_forward_online(self, x):
         x__with__bias = np.append(x, 1)
-        wn_x = np.array([wght_n * x__with__bias for wght_n in self.layer_3])
+        wn_x = np.array([wght_n * x__with__bias for wght_n in self.layer_3A])
 
         if self.layer_4_params is None:
-            self.layer_4_params = np.random.normal(0, 0.3, wn_x.shape)
+            # TODO Ones only for testing. Change it to randoms!
+            # self.layer_4_params = np.random.normal(0, 0.3, wn_x.shape)
+            self.layer_4_params = np.ones(wn_x.shape)
 
         if self.weights_4to5 is None:
             self.weights_4to5 = np.ones((wn_x.shape[0], 1))
@@ -184,6 +189,27 @@ class ANFIS1:
 
     def backprop_online(self, learning_rate=0.01):
 
+        # Layer 5
+        dE_by_W4to5, grads_l5 = self.layer_5_backprop()
+
+        # Layer 4
+        dE_by_L4Params, grads_l4 = self.layer_4_backprop(grads_l5)
+
+        # Layer 3
+        _, grads_l3 = self.layer_3_backprop(grads_l4)
+
+        # Layer 2
+        _, grads_l2 = self.layer_2_backprop(grads_l3)
+
+        # Layer 1
+        dE_by_W1, grads_l1 = self.layer_1_backprop(grads_l2)
+
+        self.layer_4_params -= learning_rate * dE_by_L4Params
+        self.weights_4to5 -= learning_rate * dE_by_W4to5
+
+        pass
+
+    def layer_5_backprop(self):
         dE_by_A5 = self.dE_by_A5
         dA5_by_Z5 = derivative(self.l5_activation_func, self.layer_5A)
         dZ5_by_W4to5 = self.layer_4A  # temporary no bias neuron
@@ -199,8 +225,9 @@ class ANFIS1:
                 ]
         ).T[0]
 
-        # 2. Prepare updates for L4 TSK params
+        return dE_by_W4to5, grads_l5
 
+    def layer_4_backprop(self, grads_l5):
         dA4_by_Z4 = derivative("linear", self.layer_4A)
         dZ4_by_L4Params = self.layer_3to4_A
 
@@ -213,79 +240,92 @@ class ANFIS1:
 
         dE_by_L4Params = np.array([grads_l4[i] * dZ4_by_L4Params[i] for i in range(len(grads_l4))])
 
-        # 3. ====================== Prepare error gradients on Layer 2 ===============================================
+        return dE_by_L4Params, grads_l4
 
-        dA2_by_Z2 = derivative("linear", self.layer_2A)
-
-        # get partial derivs by var mfs
-
-        _l2_parts_for_derivative_alloc2 = []
-        for _rule in range(self.rules.shape[0]):
-            _derivs = []
-            _a = []
-            for _var_num in range(self.rules.shape[1]):
-                _d = self.rules[_rule][_var_num]
-                _b = self.layer_1A[_var_num][_d]  # get result of fuzzy term * var input
-                _a.append(_b)
-            for _var_num in range(self.rules.shape[1]):
-                _c = copy.copy(_a)
-                _c.pop(_var_num)
-                _derivs.append(_c)
-            _l2_parts_for_derivative_alloc2.append(_derivs)
-
-        _l2_partial_derivs_by_mfs_alloc3 = []
-        for i in _l2_parts_for_derivative_alloc2:
-            _r = [np.product(derv) for derv in i]
-            _l2_partial_derivs_by_mfs_alloc3.append(_r)
-
-        dZ2_by_L1MFs = np.array(_l2_partial_derivs_by_mfs_alloc3)
+    def layer_3_backprop(self, grads_l4):
+        """
+        dA3_by_Z3: it's a derivative y'= (a * x)' where a = sum(self.layer_2A),
+        i.e. so results is y' = sum(self.layer_2A)
+        """
+        dA3_by_Z3 = np.ones(self.layer_2A.shape) * np.sum(self.layer_2A)
 
         dE_by_Z4 = grads_l4
-        dZ4_by_A2 = self.layer_2Z
+        dZ4_by_A3 = np.ones(self.layer_2A.shape)
 
-        _l2Z = np.array(self.layer_2Z)
+        dE_by_A3 = dZ4_by_A3 * dE_by_Z4
 
-        dE_by_A2 = np.array(
-            np.ones(_l2Z.shape) * np.array(np.matrix(dE_by_Z4).T)
-        )
+        grads_l3 = dE_by_A3 * dA3_by_Z3
 
-        # grads_l2 = dE_by_A2 * dA2_by_Z2
-        grads_l2 = dE_by_A2  # dA2_by_Z2 contains only zeroes, ignore it
+        return None, grads_l3
 
-        # 3. Prepare updates for L1 MF params
+    def layer_2_backprop(self, grads_l3):
+        dA2_by_Z2 = np.ones(self.layer_2A.shape)
 
-        dA1_by_Z1 = np.array([derivative("gaussian", i) for i in self.layer_1A])
-        dZ1_by_W_XtoMF = self.layer_0A
+        dE_by_Z3 = grads_l3
+        dZ3_by_A2 = np.ones(self.layer_2A.shape)
 
-        dE_by_Z2 = grads_l2
-        dZ2_by_A1 = dZ2_by_L1MFs
+        dE_by_A2 = dZ3_by_A2 * dE_by_Z3
 
-        dE_by_A1 = np.array(np.dot(np.matrix(dZ2_by_A1), np.matrix(dE_by_Z2)))[0]
+        grads_l2 = dE_by_A2 * dA2_by_Z2
 
-        grads = dE_by_A1 * dA1_by_Z1
+        return None, grads_l2
 
-        dE_by_W_XtoMF = np.array(
-            [[grads[i] * dZ1_by_W_XtoMF[j] for j in range(len(dZ1_by_W_XtoMF))] for i in range(len(grads))])
+    def layer_1_backprop(self, grads_l2):
+        # 3. ====================== Prepare error gradients on Layer 2 ===============================================
+
+        # dA2_by_Z2 = derivative("linear", self.layer_2A)
+        #
+        # # get partial derivs by var mfs
+        #
+        # _l2_parts_for_derivative_alloc2 = []
+        # for _rule in range(self.rules.shape[0]):
+        #     _derivs = []
+        #     _a = []
+        #     for _var_num in range(self.rules.shape[1]):
+        #         _d = self.rules[_rule][_var_num]
+        #         _b = self.layer_1A[_var_num][_d]  # get result of fuzzy term * var input
+        #         _a.append(_b)
+        #     for _var_num in range(self.rules.shape[1]):
+        #         _c = copy.copy(_a)
+        #         _c.pop(_var_num)
+        #         _derivs.append(_c)
+        #     _l2_parts_for_derivative_alloc2.append(_derivs)
+        #
+        # _l2_partial_derivs_by_mfs_alloc3 = []
+        # for i in _l2_parts_for_derivative_alloc2:
+        #     _r = [np.product(derv) for derv in i]
+        #     _l2_partial_derivs_by_mfs_alloc3.append(_r)
+        #
+        # dZ2_by_L1MFs = np.array(_l2_partial_derivs_by_mfs_alloc3)
+        #
+        # dE_by_Z4 = grads_l4
+        # dZ4_by_A2 = self.layer_2Z
+        #
+        # _l2Z = np.array(self.layer_2Z)
+        #
+        # dE_by_A2 = np.array(
+        #     np.ones(_l2Z.shape) * np.array(np.matrix(dE_by_Z4).T)
+        # )
+        #
+        # # grads_l2 = dE_by_A2 * dA2_by_Z2
+        # grads_l2 = dE_by_A2  # dA2_by_Z2 contains only zeroes, ignore it
+        #
+        # # 3. Prepare updates for L1 MF params
+        #
+        # dA1_by_Z1 = np.array([derivative("gaussian", i) for i in self.layer_1A])
+        # dZ1_by_W_XtoMF = self.layer_0A
+        #
+        # dE_by_Z2 = grads_l2
+        # dZ2_by_A1 = dZ2_by_L1MFs
+        #
+        # dE_by_A1 = np.array(np.dot(np.matrix(dZ2_by_A1), np.matrix(dE_by_Z2)))[0]
+        #
+        # grads = dE_by_A1 * dA1_by_Z1
+        #
+        # dE_by_W_XtoMF = np.array(
+        #     [[grads[i] * dZ1_by_W_XtoMF[j] for j in range(len(dZ1_by_W_XtoMF))] for i in range(len(grads))])
 
         # Tune params
 
-        self.weights_XtoMFs -= learning_rate * dE_by_W_XtoMF
-        self.layer_4_params -= learning_rate * dE_by_L4Params
-        self.weights_4to5 -= learning_rate * dE_by_W4to5
-
-        pass
-
-    def layer_5_backprop(self):
-        pass
-
-    def layer_4_backprop(self):
-        pass
-
-    def layer_3_backprop(self):
-        pass
-
-    def layer_2_backprop(self):
-        pass
-
-    def layer_1_backprop(self):
-        pass
+        # self.weights_XtoMFs -= learning_rate * dE_by_W_XtoMF
+        return None, None
